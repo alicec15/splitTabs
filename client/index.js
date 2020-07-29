37a382d74940fcc9a644093bca3442df77e22ed6
alicec15 <alicejychen15@gmail.com>
2020-07-28 22:07:02 -0400

    initial setup

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './pages';
+// import './stylesheets/style.scss';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+//   <App />,
+  document.getElementById('app')
+);
---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
+const PORT = process.env.PORT || 8080
+
+module.exports = app;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+});
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message || 'Internal server error')
+});
+app.listen(PORT, () => console.log('listening on port ', PORT));
+
